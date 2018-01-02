57beb29258c6934153c3d943a83d0f265565362e
shrutivanw <shruti.dalvi@gmail.com>
2018-01-02 15:58:28 -0800

    Reverse sentence
    CS Fundamentals' assignment to reverse the words in a sentence in place while preserving the count of white spaces.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# reverse_sentence
+# Reverse Sentence
+In this assignment, you'll design and implement one of the string manipulation functions that is commonly asked during interviews.
+Remember that a string is an array of characters. Algorithms that worked on restricted arrays will work on strings as well.
+
+## Exercise
+* Design and implement a method to reverse the words in a sentence *in place*. For example, if the method is called with input parameter of "Yoda&nbsp;&nbsp;&nbsp;is&nbsp;&nbsp;&nbsp;&nbsp;awesome", the method should update the input string object to have the value "awesome&nbsp;&nbsp;&nbsp;&nbsp;is&nbsp;&nbsp;&nbsp;Yoda". Note that the count of white spaces between words is preserved.
+* Share and explain the time and space complexities for your solution.
+* At minimum, your implementation should pass the basic tests.
+**Note**: Do not use Ruby provided functionality for `.reverse` and `.reverse!`. You may use `.length`.
---
+++ specs/reverse_sentence_spec.rb
@@ -0,0 +1,92 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require_relative '../lib/reverse_sentence'
+
+describe "reverse sentence" do
+  describe "basic tests" do
+    describe "test 1" do
+      it "reverse a sentence with two words" do
+        test_string = "hello, world"
+
+        reverse_sentence(test_string)
+
+        test_string.must_equal "world hello,"
+      end
+    end
+
+    describe "test 2" do
+      it "reverse a sentence with three words" do
+        test_string = "Yoda is awesome!"
+
+        reverse_sentence(test_string)
+
+        test_string.must_equal "awesome! is Yoda"
+      end
+    end
+  end
+
+  # if it's a string parameter, check for empty
+  describe "empty string input test" do
+    it "reverse an empty sentence" do
+      test_string = ""
+
+      reverse_sentence(test_string)
+
+      test_string.must_be_empty
+    end
+  end
+
+  # if the parameter is an object, check for nil
+  describe "nil object input test" do
+    it "nil object passed to reverse" do
+      test_string = nil
+
+      reverse_sentence(test_string)
+
+      test_string.must_be_nil
+    end
+  end
+
+  # check for edge cases
+  describe "edge cases" do
+    describe "edge case 1" do
+      it "reverse a sentence with one word" do
+        test_string = "world"
+
+        reverse_sentence(test_string)
+
+        test_string.must_equal "world"
+      end
+    end
+
+    describe "edge case 2" do
+      it "reverse a sentence with multiple words" do
+        test_string = "I'm a better engineer today than I was yesterday."
+
+        reverse_sentence(test_string)
+
+        test_string.must_equal "yesterday. was I than today engineer better a I'm"
+      end
+    end
+
+    describe "edge case 3" do
+      it "reverse a sentence with multiple spaces between words" do
+        test_string = "How  do  you   like     them      apples?"
+
+        reverse_sentence(test_string)
+
+        test_string.must_equal "apples?      them     like   you  do  How"
+      end
+    end
+
+    describe "edge case 4" do
+      it "reverse a sentence with preceeding and trailing white spaces" do
+        test_string = "  I can do this!     "
+
+        reverse_sentence(test_string)
+
+        test_string.must_equal "     this! do can I  "
+      end
+    end
+  end
+end
